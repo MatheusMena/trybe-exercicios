e018b34b3ee2657036fa46e1bc05dc4aa479db2d
MatheusMena <matheusmena1@hotmail.com>
2021-10-16 15:56:40 -0300

    Exercicio 1

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -0,0 +1,17 @@
+const a =10
+const b =20
+
+let adicao = a + b ;
+console.log(adicao);
+
+let subtracao = a - b;
+console.log(subtracao);
+
+let multiply = a * b ;
+console.log( multiply);
+
+let divisao = a / b ;
+console.log(divisao);
+
+let modulo = a % b ;
+console.log(modulo);
