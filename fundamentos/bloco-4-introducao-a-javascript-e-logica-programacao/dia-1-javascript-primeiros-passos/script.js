6ee21f1225a3b463b010ea73576cfd467376c349
MatheusMena <matheusmena1@hotmail.com>
2021-10-16 16:10:50 -0300

    Exercicio 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -1,17 +1,28 @@
-const a =10
-const b =20
+// const a =10
+// const b =20
 
-let adicao = a + b ;
-console.log(adicao);
+// let adicao = a + b ;
+// console.log(adicao);
 
-let subtracao = a - b;
-console.log(subtracao);
+// let subtracao = a - b;
+// console.log(subtracao);
 
-let multiply = a * b ;
-console.log( multiply);
+// let multiply = a * b ;
+// console.log( multiply);
 
-let divisao = a / b ;
-console.log(divisao);
+// let divisao = a / b ;
+// console.log(divisao);
 
-let modulo = a % b ;
-console.log(modulo);
+// let modulo = a % b ;
+// console.log(modulo);
+
+const num1 = 3;
+const num2 = 6;
+
+if( num1 > num2){
+ console.log(num1)
+}else if (num2 > num1){
+console.log(num2 +  " ,este é o numero maior") 
+}else{
+    console.log("são iguais")
+}
