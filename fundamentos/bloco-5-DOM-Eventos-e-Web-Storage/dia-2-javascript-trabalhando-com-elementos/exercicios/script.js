10fa1044c9f9d8d31d47abc4bfb011a2c8f1f0d8
MatheusMena <matheusmena1@hotmail.com>
2021-10-26 18:52:54 -0300

    exercicio 2

--- fundamentos/bloco-5-DOM-Eventos-e-Web-Storage/dia-2-javascript-trabalhando-com-elementos/exercicios/script.js
+++ fundamentos/bloco-5-DOM-Eventos-e-Web-Storage/dia-2-javascript-trabalhando-com-elementos/exercicios/script.js
@@ -1,1 +1,4 @@
-console.log(document.getElementById("elementoOndeVoceEsta"))
+//exercicio 1 console.log(document.getElementById("elementoOndeVoceEsta"))
+
+let addColor = document.getElementById("elementoOndeVoceEsta").parentNode
+addColor.style.color = "green"
