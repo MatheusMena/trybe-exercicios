7aaff2aabe362a93b2e2c679ab2d1ad57f3132ba
MatheusMena <matheusmena1@hotmail.com>
2021-10-26 18:56:13 -0300

    exercicio 3

--- fundamentos/bloco-5-DOM-Eventos-e-Web-Storage/dia-2-javascript-trabalhando-com-elementos/exercicios/script.js
+++ fundamentos/bloco-5-DOM-Eventos-e-Web-Storage/dia-2-javascript-trabalhando-com-elementos/exercicios/script.js
@@ -1,4 +1,8 @@
-//exercicio 1 console.log(document.getElementById("elementoOndeVoceEsta"))
+//exercicio 1
+//  console.log(document.getElementById("elementoOndeVoceEsta"))
 
-let addColor = document.getElementById("elementoOndeVoceEsta").parentNode
-addColor.style.color = "green"
+// exercicio 2 
+// let addColor = document.getElementById("elementoOndeVoceEsta").parentNode
+// addColor.style.color = "green"
+
+document.getElementById("primeiroFilhoDoFilho").innerText = " adicione texto aqui";
