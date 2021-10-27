a75efa7f25a4e234873d7d20f49b1874df48053e
MatheusMena <matheusmena1@hotmail.com>
2021-10-27 11:49:49 -0300

    exercicio 1

---
+++ fundamentos/bloco-5-DOM-Eventos-e-Web-Storage/dia-2-javascript-trabalhando-com-elementos/exercicios/parte2/script.js
@@ -0,0 +1,11 @@
+// 1 --- Crie um irmão para elementoOndeVoceEsta .
+
+// let novoFilho = document.getElementById("pai");
+// let filhoNovo = document.createElement('section')
+// filhoNovo.id = 'filhoNovo';
+// novoFilho.appendChild(filhoNovo);
+
+let pai = document.getElementById('pai');
+let abacadraba = document.createElement('figo');
+abacadraba.id = "abacadraba"
+pai.appendChild(abacadraba);
