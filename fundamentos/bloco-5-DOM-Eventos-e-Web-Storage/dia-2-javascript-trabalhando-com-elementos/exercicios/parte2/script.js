8fa5d15959bf2c5f63dd4e07a4a989aba1f00d9f
MatheusMena <matheusmena1@hotmail.com>
2021-10-27 12:20:41 -0300

    exercicio 3

--- fundamentos/bloco-5-DOM-Eventos-e-Web-Storage/dia-2-javascript-trabalhando-com-elementos/exercicios/parte2/script.js
+++ fundamentos/bloco-5-DOM-Eventos-e-Web-Storage/dia-2-javascript-trabalhando-com-elementos/exercicios/parte2/script.js
@@ -7,7 +7,14 @@
 
 // 2 ---Crie um filho para elementoOndeVoceEsta .
 
-let pai = document.getElementById('elementoOndeVoceEsta');
-let filhoMaisNovo = document.createElement('section')
-filhoMaisNovo.id = 'filhoMaisNovo';
-pai.appendChild(filhoMaisNovo);
+// let pai = document.getElementById('elementoOndeVoceEsta');
+// let filhoMaisNovo = document.createElement('section')
+// filhoMaisNovo.id = 'filhoMaisNovo';
+// pai.appendChild(filhoMaisNovo);
+
+// 3---Crie um filho para primeiroFilhoDoFilho .
+
+let pai = document.getElementById("primeiroFilhoDoFilho");
+let filhoFilho = document.createElement('section');
+filhoFilho.id = "filhoFilho";
+pai.appendChild(filhoFilho);
