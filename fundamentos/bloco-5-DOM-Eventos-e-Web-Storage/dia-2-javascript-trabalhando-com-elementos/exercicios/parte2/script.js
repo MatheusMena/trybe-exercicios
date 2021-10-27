b4edfbebdc67723876f93e1c7db3aeba745e18ad
MatheusMena <matheusmena1@hotmail.com>
2021-10-27 17:33:04 -0300

    exercicio 1 ao 3

--- fundamentos/bloco-5-DOM-Eventos-e-Web-Storage/dia-2-javascript-trabalhando-com-elementos/exercicios/parte2/script.js
+++ fundamentos/bloco-5-DOM-Eventos-e-Web-Storage/dia-2-javascript-trabalhando-com-elementos/exercicios/parte2/script.js
@@ -28,4 +28,17 @@
 
 // exercicio 1
 
-Remova todos os elementos filhos de paiDoPai exceto pai , elementoOndeVoceEsta e primeiroFilhoDoFilho
+// Remova todos os elementos filhos de paiDoPai exceto pai , elementoOndeVoceEsta e primeiroFilhoDoFilho;
+// ( não entendi essa...)
+
+// const pai = document.getElementById('pai');
+
+// for (let index = pai.childNodes.length - 1; index >= 0; index -= 1) {
+//   const currentChildren = pai.childNodes[index];
+//   if (currentChildren.id !== 'elementoOndeVoceEsta') {
+//     currentChildren.remove();
+//   }
+// }
+
+// const segundoEUltimoFilhoDoFilho = document.getElementById('segundoEUltimoFilhoDoFilho');
+// segundoEUltimoFilhoDoFilho.remove();
